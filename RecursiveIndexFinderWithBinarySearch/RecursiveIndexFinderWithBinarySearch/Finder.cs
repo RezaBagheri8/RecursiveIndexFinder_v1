85fb19275e6078c06930b1f6d71425c12cf8f9cd
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-28 10:01:29 +0430

    Initial Repo

---
+++ RecursiveIndexFinderWithBinarySearch/RecursiveIndexFinderWithBinarySearch/Finder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveIndexFinderWithBinarySearch
+{
+    public class Finder
+    {
+        public int Find(int[] array, int first, int last, int key)
+        {
+            int middle = (first + last) / 2;
+
+            if (first > last)
+            {
+                return first;
+            }
+            else if (first == last)
+            {
+                if (key < array[first])
+                {
+                    return last;
+                }
+                else
+                {
+                    return last + 1;
+                }
+            }
+            else if (key == array[middle])
+            {
+                return middle + 1;
+            }
+            else if (key > array[middle])
+            {
+                return Find(array, middle + 1, last, key);
+            }
+            else
+            {
+                return Find(array, first, middle - 1, key);
+            }
+        }
+    }
+}
---
+++ RecursiveIndexFinderWithBinarySearch/RecursiveIndexFinderWithBinarySearch/Program.cs
@@ -0,0 +1,9 @@
+﻿using RecursiveIndexFinderWithBinarySearch;
+
+int[] array = { 0, 1, 2, 3, 4, 7, 45, 90, 10000 };
+
+Finder finder = new Finder();
+
+int index = finder.Find(array, 0, array.Length - 1, 200000000);
+
+Console.WriteLine(index);
